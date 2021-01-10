e0f50e72bd69880ca57abdf2e28b8f096da3a396
Izumi-No <67838060+Izumi-No@users.noreply.github.com>
2021-01-10 02:41:37 -0300

    Add files via upload

--- app/src/pages/sign in/style.jsx
+++ app/src/pages/sign in/style.jsx
@@ -15,156 +15,228 @@ margin: -8px;
     margin: auto;
     width: 100%;
     height: 100%;
+    filter: saturate(0);
+
 }
 
 /* formulario */
 .background-principal{
-    width: 35vmax;
-    height: 45vmax;
-
+    width: 570px ;
+    height: 670px ;
     position: fixed;
     top: 0; bottom: 0;
-    left: 0; right: 0;
+    left: 0; right: 950px;
     margin: auto;
-
-    border-radius: 0.7vmax;
-    background: rgba(0, 0, 0, 0.35);
+    border-radius: 5px;
+    background: rgba(0, 0, 0, 0.45);
 }
 
 .entrar{
     font-family: Roboto;
-    font-size: 2.2vmax;
-    margin-top: 3vmax;
-    margin-bottom: 3vmax;
-    margin-left: 2.5vmax;
-
+    font-size: 30px;
+    margin: 0;
+    margin-left: 60px;
+    margin-top: 65px;
+    margin-bottom: 70px;
     color: #ffffff;
-
-    font-weight: 300;
+    font-weight: 350;
 }
 
 /*input de email*/
 .email_or_number{
-    width: 30vmax;
-    height: 4vmax;
-    margin-top: 4vmax;
-    margin-left: auto;
-    margin-right: auto;
-    top: 0; bottom: 0;
-    left: 0; right: 0;
-    display: flex;
-
-    font-size: 1.5vmax;
-    color: #ffffff;
+    width: 434px;
+    height: 65px;
+    padding-left: 16px;
+    margin: 0;
+    color: #BDBDBD;
+    font-size: 18px;
+    background-color: #535353;
+    border-style: none;
+    border-radius: 5px;
 }
 
 input[type="email number"]{
-    background-color: #535353;
-    border-style: none;
-
-    border-radius: 0.5vmax;
+    display: flex;
+    margin-bottom: 40px;
+    margin-left: auto;
+    margin-right: auto;
+    
 }
 
 input[type="email number"]::placeholder{
-    padding-left: 2vmax;
+    align-items: center;
+    justify-content: center;
     font-family: Roboto;
-    font-weight: 400;
-    font-size: 0.9vmax;
+    font-weight: 300;
+    font-size: 18px;
     color: #ffffffa1;
 }
 
 /*input de senha*/
-.password{
-    width: 30vmax;
-    height: 4vmax;
+.password1 {
+    width: 434px;
+    height: 65px;
+    padding-left: 16px;
+    display: flex;
+    margin-bottom: 40px;
+    margin-top: -18px;
     margin-left: auto;
     margin-right: auto;
-    top: 0; bottom: 0;
-    left: 0; right: 0;
-    display: flex;
-
-    font-size: 1.5vmax;
-    color: #ffffff;
-
-    border-radius: 0.5vmax;
-}
-
-
-input[type="password"]{
+    color: #BDBDBD;
+    font-size: 18px;
     background-color: #535353;
     border-style: none;
-
-    border-radius: 0.5vmax;
+    border-radius: 5px;
+    
 }
 
 input[type="password"]::placeholder{
-    padding-left: 1.5vmax;
     align-items: center;
     justify-content: center;
     font-family: Roboto;
-    font-weight: 400;
-    font-size: 0.9vmax;
+    font-weight: 300;
+    font-size: 18px;
     color: #ffffffa1;
+    
 }
 
 /* buttons */
 
-.button_entrar{
-    width: 30vmax;
-    height: 4.2vmax;
-    margin-top: 2vmax;
-    margin-left: auto;
-    margin-right: auto;
-    top: 0; bottom: 0;
-    left: 0; right: 0;
-    display: flex;
-
-    border-radius: 0.5vmax;
-
-    background-color: #E30000;
-    border-style: none;
-
-    color: #ffffff;
-    font-size: 1.5vmax;
-    align-items: center;
-    justify-content: center;
-
-    font-family: Roboto;
-    font-weight: 300;
-}
-
-.link-1 { 
+.link-1, .link-2 {
     text-decoration: none;
 }
 
-.link-2 { 
-    text-decoration: none;
-}
-
-.button_criarConta{
-    width: 30vmax;
-    height: 4.2vmax;
+.button_entrar {
+    width: 450px;
+    height: 70px;
+    display: flex;
+    margin-top: -18px;
+    margin-bottom: 35px;
     margin-left: auto;
     margin-right: auto;
-    top: 0; bottom: 0;
-    left: 0; right: 0;
-    display: flex;
-
-    
-
-    border-radius: 0.5vmax;
-
-    background-color: #484848;
+    border-radius: 5px;
+    background-image: linear-gradient(113deg, red 0%, yellow 49%, blue 52%, cyan 100%);
     border-style: none;
-
     color: #ffffff;
-    font-size: 1.5vmax;
+    font-size: 18px;
     align-items: center;
     justify-content: center;
-
     font-family: Roboto;
-    font-weight: 300;
+    font-weight: 350;
+    transition: all .5s ease;
+
+    background-size: 300% 100%;
+
 }
+.button_entrar:hover{
+    background-position: 100% 0;
+    transition: all .5s ease;
+    cursor: pointer 
+
+  }
+
+.button_criarConta {
+    width: 450px;
+    height: 70px;
+    display: flex;
+    margin-top: -18px;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 5px;
+    background: linear-gradient(113deg, #484848 0%, #484848 50%, blue 51%, cyan 100%);
+    border-style: none;
+    color: #ffffff;
+    font-size: 18px;
+    align-items: center;
+    justify-content: center;
+    font-family: Roboto;
+    font-weight: 350;
+    background-size: 300% 100%;
+    transition: all .5s ease
+}
+.button_criarConta:hover{
+    background-position: 100% 0;
+    transition: all .5s ease;
+    cursor: pointer 
+
+}
+
+@media (max-width: 1750px) {
+    .background-principal{
+        right: 600px;
+    }
+}
+
+@media (max-width: 1250px) {
+    .background-principal{
+        width: 570px ;
+        height: 670px ;
+        position: fixed;
+        right: 0;
+    }
+}
+@media (max-width: 650px) {
+    .background-principal{
+        width: 90vw;
+        height: 60vh;
+        display: block;
+    }
+
+    .entrar{
+        margin-left: 5vh;
+        margin-top: 6vh;
+        margin-bottom: 10vw;
+
+        font-size: 2.5vmax;
+        font-weight: 300;
+    }
+
+    .email_or_number{
+        width: 70vw;
+        height: 6vmax;
+        font-size: 2vmax;
+    }
+
+    input[type="email number"]{
+        margin-bottom: 2vmax;
+    }
+
+    input[type="email number"]::placeholder{
+        font-size: 2vmax;
+    }
+
+    .password1 {
+        width: 70vw;
+        height: 6vmax;
+        font-size: 2vmax;
+        margin-bottom: 2.5vmax;
+    }
+
+    .password2 {
+        width: 70vw;
+        height: 6vmax;
+        font-size: 2vmax;
+        margin-bottom: 0;
+    }
+
+    input[type="password"]::placeholder{
+        font-size: 2vmax;
+    }
+
+    .button_entrar{
+        width: 75vw;
+        height: 6vmax;
+        font-size: 2vmax;
+        margin-bottom: 2.5vmax
+    }
+
+    .button_criarConta {
+        width: 75vw;
+        height: 6vmax;
+        font-size: 2vmax;
+    }
+}
+
 `;
 
 export default Bodyy;
