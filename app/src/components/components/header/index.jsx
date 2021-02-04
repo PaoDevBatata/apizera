15bc84020cbedf15de1d62ad996f7d9c646e2732
PaoDevBatata <PaoDevBatata24@gmail.com>
2021-02-04 17:50:49 -0300

    barra bugada

--- app/src/components/components/header/index.jsx
+++ app/src/components/components/header/index.jsx
@@ -6,11 +6,27 @@ function Header() {
   return (
     <Container >
       <div className="group_A">
-        <div className="hamburguer">
-          <div className="barra1 ham"></div>
-          <div className="barra2 ham"></div>
-          <div className="barra3 ham"></div>
-        </div>
+      <nav role="navigation">
+  <div id="menuToggle">
+    
+    <input type="checkbox" />
+    
+   
+    
+    <span></span>
+    <span></span>
+    <span></span>
+    
+    
+    <ul id="menu">
+      <a href="#"><li>Home</li></a>
+      <a href="#"><li>About</li></a>
+      <a href="#"><li>Info</li></a>
+      <a href="#"><li>Contact</li></a>
+      <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
+    </ul>
+  </div>
+</nav>
         <input type="text" className="buscar" placeholder="buscar" />
       </div>
     </Container>
